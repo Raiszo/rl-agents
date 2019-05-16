7eaa14e86b041ae62ac7a5647707e2c137ef393a
Raiszo <E05.sek@gmail.com>
2019-05-15 20:21:42 -0500

    :dagger: adding notebook + Sampling as a custom layer :3

--- rl_agents/ppo/policy.py
+++ rl_agents/ppo/policy.py
@@ -1,46 +1,57 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow_probability import distributions as dist
+from tensorflow_probability import distributions as dists
 import tensorflow.keras.layers as kl
 
+
+class ContinuousSample(kl.Layer):
+    def __init__(self, action_dim, log_std=-0.53):
+        super().__init__(name='ContinuousSample')
+
+        s_init = tf.constant_initializer(np.exp(log_std))
+        self.std = tf.Variable(initial_value=s_init(shape=(1, action_dim),
+                                                    dtype='float32'),
+                               trainable=True)
+
+    def call(self, logits):
+        # No need to add zeros lie logis, this shit should be broadcasted
+        distribution = dists.Normal(loc=logits, scale=self.std)
+        
+        sample = distribution.sample()
+        log_prob = distribution.log_prob(sample)
+
+        return sample, log_prob, distribution, self.std
+        
 
 class Actor(tf.keras.Model):
     def __init__(self, obs_space, act_space, is_continuous, size=32, num_layers=2):
         super().__init__(name='Actor')
         self.continuous = is_continuous
 
-        print(obs_space)
         self.layer_1 = kl.Dense(size, input_shape=obs_space, activation=tf.nn.relu)
         self.layer_2 = kl.Dense(size, activation=tf.nn.relu)
         
         # Logits
         self.logits = kl.Dense(act_space[0])
+
+        # Sample
+        self.sample = ContinuousSample(act_space[0])
         
-        self.log_std = log_std = -0.53 * tf.ones([1, act_space[0]])
         
     def call(self, inputs):
         # x = tf.convert_to_tensor(inputs, dtype=tf.float32)
         x = self.layer_1(inputs)
         x = self.layer_2(x)
         logits = self.logits(x)
+        sample = self.sample(logits)
 
-        return logits
+        return sample
 
     def action(self, obs):
-        logits = self.predict(obs)
-        print(logits)
-        
-        std = tf.zeros_like(logits) + tf.exp(self.log_std)
-        
-        distribution = dist.Normal(loc=logits, scale=std)
-        sample = distribution.sample().numpy()
-        log_prob = distribution.log_prob(sample).numpy()
-        
-        # return self.log_std
-    
-        return sample, log_prob
-        # return self.predict(obs)
-    
+        res = self.predict(obs)
+        print(res)
+
+        return res
 
 # class Critic(tf.keras.Model):
 #     def __init__(self, obs_space):
