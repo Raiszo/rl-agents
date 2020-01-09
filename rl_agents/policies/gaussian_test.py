874246415d94913d8458acb6f67231489ce03f79
Raiszo <E05.sek@gmail.com>
2020-01-08 19:28:23 -0500

    :dagger: just use float32 for now

--- rl_agents/policies/gaussian_test.py
+++ rl_agents/policies/gaussian_test.py
@@ -1,15 +1,14 @@
-from rl_agents.policies.categorical import CategoricalActor, CategoricalSample
+from rl_agents.policies.gaussian import GaussianActor, ContinuousSample
 from tensorflow_probability import distributions
 
 import tensorflow as tf
 import numpy as np
 
-
 class LayerTest(tf.test.TestCase):
 
     def setUp(self):
         super(LayerTest, self).setUp()
-        self.layer = CategoricalSample()
+        self.layer = ContinuousSample((3,))
 
     def testShape(self):
         x = np.array([
@@ -22,7 +21,7 @@ class LayerTest(tf.test.TestCase):
 
         self.assertShapeEqual(np.zeros((2,3)), pi) # should be 0s and 1s
         self.assertShapeEqual(np.zeros((2,)), logp_pi)
-        self.assertIsInstance(dist, distributions.Multinomial)
+        self.assertIsInstance(dist, distributions.Normal)
         # self.assertAllEqual(x, inputs)
 
 if __name__ == '__main__':
