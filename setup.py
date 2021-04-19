816931ef8bf3858b7e020583aaf0f6b2c335635f
Raiszo <E05.sek@gmail.com>
2021-04-19 03:04:50 -0500

    dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-setup(name='rl-agents',
-      version='0.0.1',
-      install_requires=['numpy', 'tensorflow', 'tensorlfow_probability']
+setup(
+    name='rl-agents',
+    version='0.0.1',
+    install_requires=[
+        'tensorflow',
+        'tensorlfow_probability',
+        'gym[box2d]',
+        'tensorboard',
+    ]
 )
